644271b45410f9ce17ad87982908862c2da8332d
astrofrog <thomas.robitaille@gmail.com>
2009-10-09 20:16:47 +0000

    - Initial commit

---
+++ idlsave/__init__.py
@@ -0,0 +1,3 @@
+from idlsave import read
+
+__version__ = '0.9.0'
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='IDLSave',
+      version='0.9.0',
+      description='idlsave - a module to read IDL save files into Python',
+      author='Thomas Robitaille',
+      author_email='robitaille@users.sourceforge.net',
+      url='http://idlsave.sourceforge.net/',
+      packages=['idlsave'],
+     )
