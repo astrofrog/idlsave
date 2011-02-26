74a8505ba73ddf58c8193323af68c95e40912642
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-02-25 20:35:42 -0500

    Added 'Fork me on GitHub' banner

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
    <body>
       <div id="main">
          
+         <a href="http://github.com/astrofrog/idlsave"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" /></a>
          <div class='title'>IDLSave - a python module to read IDL 'save' files</div><br>
          
          <div class='subtitle'>News!</div><br>
