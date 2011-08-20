7263ba01325cd4a7b8ca2823ec47ddaf6174486e
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-08-20 18:57:54 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from distutils.core import setup
 
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
 setup(name='IDLSave',
       version='0.9.7',
-      description='idlsave - a module to read IDL save files into Python',
+      description='IDLSave - a module to read IDL save files into Python',
       author='Thomas Robitaille',
-      author_email='robitaille@users.sourceforge.net',
-      url='http://idlsave.sourceforge.net/',
+      author_email='thomas.robitaille@gmail.com',
+      url='https://github.com/astrofrog/idlsave',
       packages=['idlsave'],
+      provides=['idlsave'],
+      cmdclass={'build_py': build_py},
+      keywords=['Scientific/Engineering'],
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                  ],
      )
